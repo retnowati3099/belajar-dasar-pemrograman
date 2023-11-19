71099e000c04609053afb6bba0f19eb1aae032a9
Retno wati <retnowati3099@gmail.com>
2023-11-20 00:07:39 +0700

    declaration of constants in single line

--- Golang/constant/constant.go
+++ Golang/constant/constant.go
@@ -12,7 +12,12 @@ const (
 	A int = 1
 	B     = 3.14
 	C     = "Hello"
+	D
 )
+
+// deklarasi lebih dari satu kosntanta dalam satu baris
+const satu, dua = 1, 2
+const tiga, empat string = "tiga", "empat"
 
 func main() {
 	fmt.Println(PI)
@@ -21,10 +26,16 @@ func main() {
 	fmt.Println(A)
 	fmt.Println(B)
 	fmt.Println(C)
+	fmt.Println(D)
+
+	fmt.Println(satu)
+	fmt.Println(dua)
+	fmt.Println(tiga)
+	fmt.Println(empat)
 }
 
 // Penulisan sintaks konstanta
-// const CONATNAME type = value
+// const CONSTNAME type = value
 
 // Aturan umum untuk konstata
 // 1. Nama konstanta mengikuti aturan penamaan pada variabel
@@ -35,3 +46,7 @@ func main() {
 // Tipe konstanta:
 // 1. Typed constants -> dideklarasi dengan tipe yang terdefinisi
 // 2. Untyped constants -> dideklarasi tanpa menyertakan tipenya. berarti tipenya mengacu dari nilai (compilermemutuskan tipe konstanta berdasarkan pada nilai)
+
+/*
+Jika tipe data dan nilai tidak dituliskan dalam deklarasi konstanta, maka tipe data dan nilai yang digunakan sama seperti kosntanta yang dideklarasikan di atasnya
+*/
