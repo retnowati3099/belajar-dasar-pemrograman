1068954c67454dd74f268a0ec5e224398049b816
Retno wati <retnowati3099@gmail.com>
2023-12-03 20:02:46 +0700

    make star pettern

--- JavaScript/pola-bintang.js
+++ JavaScript/pola-bintang.js
@@ -2,11 +2,14 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 let baris = parseInt(prompt("Masukkan banyak baris: "));
 
-// for (let i = 1; i <= baris; i++){
-//     for (let j = 1; j <= i; j++) {
-//         console.log("*");
-//     }
-// }
+for (let i = 1; i <= baris; i++) {
+	for (let j = 1; j <= i; j++) {
+		process.stdout.write("* ");
+	}
+	console.log();
+}
+
+console.log("===========");
 
 let bintang = "";
 for (let i = 1; i <= baris; i++) {
@@ -15,3 +18,16 @@ for (let i = 1; i <= baris; i++) {
 }
 
 console.log("===========");
+
+for (let i = 1; i <= baris; i++) {
+	// spasi
+	for (let j = baris - 1; j >= i; j--) {
+		process.stdout.write(" ");
+	}
+
+	// bintang
+	for (let k = 1; k <= i; k++) {
+		process.stdout.write("* ");
+	}
+	console.log();
+}
