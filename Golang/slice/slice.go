fb54d26e31c88d2f8ae0fa24a7b333e71c13b1b7
Retno wati <retnowati3099@gmail.com>
2023-12-24 22:14:48 +0700

    learn slice

--- Golang/slice/slice.go
+++ Golang/slice/slice.go
@@ -22,6 +22,37 @@ func main() {
 	fmt.Println(animals[:2]) //semua elemen hingga sebelum indeks ke-2
 
 	// fmt.Println(animals[4:0]) error pada penulisan, nilai depan harus lebih kecil atau sama dengan belakang
+
+	var drinks = []string{"freshwater", "milk", "tea", "coffee"}
+	var aDrinks = drinks[0:3]
+	var bDrinks = drinks[1:4]
+	var aaDrinks = aDrinks[1:2]
+	var baDrinks = bDrinks[0:1]
+
+	fmt.Println(drinks)
+	fmt.Println(aDrinks)
+	fmt.Println(bDrinks)
+	fmt.Println(aaDrinks)
+	fmt.Println(baDrinks)
+
+	// milk diubah menjadi juice
+	baDrinks[0] = "juice"
+
+	fmt.Println(drinks)
+	fmt.Println(aDrinks)
+	fmt.Println(bDrinks)
+	fmt.Println(aaDrinks)
+	fmt.Println(baDrinks)
+
+	// fungsi len() untuk menghitung jumlah elemen slice
+	fmt.Println(len(drinks))
+
+	// fungsi append() untuk menambahkan elemen pada slice, diposisikan setelah indeks paling akhir
+
+	var cDrinks = append(drinks, "wine")
+
+	fmt.Println(drinks)
+	fmt.Println(cDrinks)
 }
 
 /*
