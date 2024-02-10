62d1c398f1c4a76d9d21f46afc8a56f48f15e476
Retno wati <retnowati3099@gmail.com>
2024-02-10 21:51:12 +0700

    create multiple variable

--- Golang/review-materi/variabel.go
+++ Golang/review-materi/variabel.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// deklarasi banyak variabel di luar fungsi main
+var (
+	firstName = "Retno"
+	lastName  = "Wati"
+)
+
 func main() {
 	var name string
 	name = "Retno Wati"
@@ -15,4 +21,15 @@ func main() {
 
 	fullName = "Aska Putra Pratama"
 	fmt.Println(fullName)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	// deklarasi banyak variabel di dalam fungsi main
+	var (
+		fruit1 = "pineapple"
+		fruit2 = "banana"
+	)
+	fmt.Println(fruit1)
+	fmt.Println(fruit2)
 }
