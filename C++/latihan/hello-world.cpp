188d0b53549d04810cb899377d3c11794d7f25ea
Retno wati <retnowati3099@gmail.com>
2024-03-11 17:09:04 +0700

    print hello world and write comments

---
+++ C++/latihan/hello-world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Hello World!\n";
+    cout << "Hello World!";
+
+    // komentar satu baris
+
+    /*
+    komentar banyak baris
+    */
+}
