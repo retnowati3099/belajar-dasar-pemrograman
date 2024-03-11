6944fc95653141dafefed05c5a31ddcc69edb6ec
Retno wati <retnowati3099@gmail.com>
2024-03-11 17:09:41 +0700

    print hello world and write comments

---
+++ JavaScript/latihan/hello-world.js
@@ -0,0 +1,9 @@
+console.log("Hello World!") // ganti baris
+process.stdout.write("Hello World!") // tidak ganti baris
+
+// komentar satu baris
+
+/* 
+Komentar
+banyak  baris
+*/
---
+++ Python/latihan/hello-world.py
@@ -0,0 +1,15 @@
+print("Hello World") # ganti baris
+print("Hello World", end="") # tidak ganti baris
+
+# komentar satu baris
+
+'''
+komentar
+banyak baris
+'''
+
+"""
+ini juga 
+komentar
+banyak baris
+"""
