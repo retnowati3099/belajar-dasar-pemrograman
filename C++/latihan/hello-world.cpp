901c35d90438d745cf24d175d61b48d8ffedd952
Retno wati <retnowati3099@gmail.com>
2024-03-17 09:28:08 +0700

    how to print Hello World!

--- C++/latihan/hello-world.cpp
+++ C++/latihan/hello-world.cpp
@@ -13,4 +13,5 @@ int main()
     /*
     komentar banyak baris
     */
+    return 0;
 }
---
+++ Golang/latihan/hello-world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!") // menghasilkan baris baru di akhir
+	fmt.Printf("Hello World!") // tidak menghasilkan baris baru di akhir
+	fmt.Print("Hello World!") // tidak menghasilkan baris baru di akhir
+
+	// komentar satu baris
+
+	/*
+	komentar banyak
+	baris
+	*/
+}
